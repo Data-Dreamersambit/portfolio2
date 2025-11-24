854d3c6c6cd4003ef29b96e98cd8aa1f9dec977a
Sambit Sahoo <sambitsahoo191@gmail.com>
2025-11-24 18:16:56 +0530

    About fixed

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -35,7 +35,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href=""
+              
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
@@ -60,16 +60,16 @@ export const AboutSection = () => {
             </div>
             <div className="gradient-border p-6 card-hover">
               <div className="flex items-start gap-4">
-                {/* <div className="p-3 rounded-full bg-primary/10">
+                <div className="p-3 rounded-full bg-primary/10">
                   <User className="h-6 w-6 text-primary" />
-                </div> */}
-                {/* <div className="text-left">
+                </div>
+                <div className="text-left">
                   <h4 className="font-semibold text-lg">UI/UX Design</h4>
                   <p className="text-muted-foreground">
                     Designing intuitive user interfaces and seamless user
                     experiences.
                   </p>
-                </div> */}
+                </div>
               </div>
             </div>
             <div className="gradient-border p-6 card-hover">
